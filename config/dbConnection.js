da5cfac6ccecdd674b205a98df3810261f1c77a4
vinay 3elitist <vinay.3elitist@gmail.com>
2024-06-03 19:06:06 +0530

    docs: documentation added in response serivce file

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`${connect.connection.host} Successfully connected to the ${connect.connection.name} database`);
-    } catch (err) {
-        console.error("Error occured in DB Connection");
-        process.exit(1);
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(
+      `${connect.connection.host} Successfully connected to the ${connect.connection.name} database`
+    );
+  } catch (err) {
+    console.error("Error occured in DB Connection");
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- helper/response.service.js
+++ helper/response.service.js
@@ -1,23 +1,44 @@
 const { codes } = require("./responseCodes");
 
 class ResponseService {
-    static send(options, res){
-        const statusCode = options.status[0];
-        const statusText = options.status[1];
-        const message = options.message || null;
-        const response = {};
+  /**
+   * Sends a response to the client with the provided options.
+   *
+   * @param {Object} options - The options for the response.
+   * @param {Array} options.status - The HTTP status code and text.
+   * @param {string} [options.message] - The message to include in the response.
+   * @param {Object} [options.data] - The data to include in the response.
+   * @param {Object} res - The Express response object.
+   *
+   * @returns {void}
+   */
+  static send(options, res) {
+    const statusCode = options.status[0];
+    const statusText = options.status[1];
+    const message = options.message || null;
+    const response = {};
 
-        response.code = statusCode;
-        response.message = message;
-        response.data = options.data || "No Data Available";
-        response.status = statusText;
+    response.code = statusCode;
+    response.message = message;
+    response.data = options.data || "No Data Available";
+    response.status = statusText;
 
-        res.status(statusCode).send(response);
-    }
+    res.status(statusCode).send(response);
+  }
 
-    static getCode(code){
-        return codes[code];
-    }
+  /**
+   * Retrieves the corresponding status code and text from the provided code.
+   *
+   * @param {string} code - The code to retrieve the status for.
+   * @returns {Array} - An array containing the HTTP status code and text.
+   *
+   * @example
+   * const statusCodeAndText = ResponseService.getCode('SUCCESS');
+   * console.log(statusCodeAndText); // Output: [200, 'Success']
+   */
+  static getCode(code) {
+    return codes[code];
+  }
 }
 
 module.exports = ResponseService;
